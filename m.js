6ae379058480e9cdd28b98673eeab0cf028fcf14
Martin <office@martinpagels.de>
2022-04-14 10:11:50 +0200

    add find student by id

--- m.js
+++ m.js
@@ -11,7 +11,16 @@ const Student = mongoose.model("student", studentSchema);
 
 mongoose.connect("mongodb://localhost:27017/neuefische").then(() => {
   console.log("connected");
-  Student.find({}).then((data) => {
+
+  // get all students back
+  /*   Student.find({}).then((data) => {
+    console.log(data);
+    process.exit();
+  }); */
+
+  // get a student by id
+
+  Student.findById("625753923730a4b24b5ce9eb").then((data) => {
     console.log(data);
     process.exit();
   });
