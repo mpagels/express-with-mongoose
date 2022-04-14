4619b3bcbf5bcc9bcc63b3d9db21f8062ba08dd8
Martin <office@martinpagels.de>
2022-04-14 11:01:17 +0200

    add get students back from mongo when hitting "/" path

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
+const Student = require("./models/student.js");
+
 const app = express();
 const port = 3000;
 
+mongoose.connect("mongodb://localhost:27017/neuefische");
+
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  Student.find({}).then((data) => {
+    res.send(data);
+  });
 });
 
 app.listen(port, () => {
