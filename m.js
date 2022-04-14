760673d6a1db43ac663d2e30edac803f32993ec8
Martin <office@martinpagels.de>
2022-04-14 10:18:24 +0200

    add add student logic

--- m.js
+++ m.js
@@ -20,7 +20,21 @@ mongoose.connect("mongodb://localhost:27017/neuefische").then(() => {
 
   // get a student by id
 
-  Student.findById("625753923730a4b24b5ce9eb").then((data) => {
+  /*  Student.findById("625753923730a4b24b5ce9eb").then((data) => {
+    console.log(data);
+    process.exit();
+  }); */
+
+  // create a student and save in database
+
+  const newStudent = Student({
+    name: "Alex",
+    age: 18,
+    points: 0,
+    happiness: 8,
+  });
+
+  newStudent.save().then((data) => {
     console.log(data);
     process.exit();
   });
